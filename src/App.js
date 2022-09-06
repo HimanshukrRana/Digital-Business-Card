5578498162f5552fd7b35cb221eae549e8524634
Himanshu Rana <hr.himanshu143@gmail.com>
2022-09-06 14:06:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,37 +15,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="App-desc">
+        <img src={profile_pic} alt="Profile-pic" className="profile-pic" />
+        <div className="intro-section">
+          <div className="intro-desc">
+            <div className="intro">
+              <h2 className="name">Laura Smith</h2>
+              <h3 className="profession">Frontend Developer</h3>
+              <h4 className="website">laurasmith.website</h4>
+            </div>
 
-      <img src={profile_pic} alt="Profile-pic" className="profile-pic" />
-      <div className="intro-section">
-        <div className="intro">
-          <h2 className="name">Laura Smith</h2>
-          <h3 className="profession">Frontend Developer</h3>
-          <h4 className="website">laurasmith.website</h4>
+            <div className="btn-social">
+              <button className="btn-email btn"><img src={email} alt="mail" />Email</button>
+              <button className="btn-linkd btn"><img src={linkd} alt="in" />LinkedIn</button>
+            </div>
+
+          </div>
+
+          <div className="summary">
+            <h3 className="summary-title">About</h3>
+            <p className="summary-text">
+              I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+            </p>
+            <h3 className="interests-title">Interests</h3>
+            <p className="interests-text">
+              Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.          </p>
+          </div>
+
+
         </div>
 
-        <div className="btn">
-          <button className="btn-email"><img src={email} alt="mail" />Email</button>
-          <button className="btn-linkd"><img src={linkd} alt="in" />LinkedIn</button>
+        <div className="social-icon">
+          <img src={Twitter} alt="twitter" />
+          <img src={Instagram} alt="twitter" />
+          <img src={Facebook} alt="twitter" />
+          <img src={GitHub} alt="twitter" />
         </div>
-
-        <div className="summary">
-          <h3 className="summary-title">About</h3>
-          <p className="summary-text">
-            I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
-          </p>
-          <h3 className="interests-title">Interests</h3>
-          <p className="interests-text">
-            Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.          </p>
-        </div>
-
-      </div>
-
-      <div className="social-icon">
-        <img src={Twitter} alt="twitter" />
-        <img src={Instagram} alt="twitter" />
-        <img src={Facebook} alt="twitter" />
-        <img src={GitHub} alt="twitter" />
       </div>
     </div>
   );
